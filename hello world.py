2ccfa821016a6a5d551dc92908dd676a9785e205
jysun32 <85902113+jysun32@users.noreply.github.com>
2021-06-19 13:08:44 -0700

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,26 @@
+print ("Hello world!")
+
+name = "LeBron" #text sequence
+print(name)
+
+age = 35 #numeric / integer
+print(age)
+
+salary = 37.44 #numeric / float
+print(salary)
+
+plays_basketball = True #boolean
+print(plays_basketball)
+
+jersey_number = "23"
+print(jersey_number)
+
+car_name = "Ram"
+print(car_name)
+
+x = 50
+print(x)
+
+myFirstName = "Jia Yong"
+print(myFirstName)
+
