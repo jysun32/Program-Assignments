074199d9bb486e608b68e8663ec49d3d66168669
jysun32 <85902113+jysun32@users.noreply.github.com>
2021-06-20 21:47:50 -0700

    Add files via upload

--- dictionaryPractice.py
+++ dictionaryPractice.py
@@ -1,4 +1,4 @@
-groceries = {"chicken": "$1.59", "beef": "$1.99", "cheese": "$1.00", "milk": "$2.50"}
+groceries = {"chicken": 1.59, "beef": 1.99, "cheese": 1.00, "milk": 2.5}
 
 chicken_price = groceries["chicken"]
 chicken2_price = groceries["chicken"]
@@ -20,3 +20,18 @@ print(chicken_price)
 print(beef_price)
 print(cheese_price)
 print(milk_price)
+
+def totalPrice(price1, price2):
+    addPrice = price1 + price2
+    return addPrice
+
+def differenceInPrice(price1, price2):
+    subtractPrice = price1 - price2
+    if subtractPrice < 0:
+        return -subtractPrice
+    else: 
+        return subtractPrice 
+
+print(totalPrice(chicken_price, beef_price))
+print(differenceInPrice(chicken_price, beef_price))
+print(differenceInPrice(beef_price, cheese_price))
